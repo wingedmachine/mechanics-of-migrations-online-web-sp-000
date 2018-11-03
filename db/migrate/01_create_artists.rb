5eb3e54c6524d924810370d22ac227210c4acdb4
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 00:48:03 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -6,7 +6,11 @@ class CreateArtists < ActiveRecord::Base
   end
 
   def change
-    create_table :artists do |t|
+    create_table :artists do |t|\
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
     end
   end
 end
